f526eb2c41af04be722781a8e346fbc7b2917cfc
VENUS27 <aliuzung@gmail.com>
2020-11-14 10:23:49 -0500

    last change from boylug

--- src/test/java/pages/US_014.java
+++ src/test/java/pages/US_014.java
@@ -1,4 +1,17 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class US_014 {
+
+   @FindBy(linkText = "My Operations")
+    public WebElement myOperation;
+
+   @FindBy(linkText = "Manage Customers")
+    public WebElement manageCustomers;
+
+   @FindBy(linkText = "Create a new Customer")
+    public WebElement newCustomer;
+
 }
--- src/test/java/stepdefinitions/TC100_StepDefinitions.java
+++ src/test/java/stepdefinitions/TC100_StepDefinitions.java
@@ -1,11 +1,11 @@
 package stepdefinitions;
 
+import org.junit.Test;
+import org.openqa.selenium.support.ui.Select;
 import utilities.ConfigurationReader;
 import utilities.Driver;
 
 public class TC100_StepDefinitions {
-
-
 
 
 
