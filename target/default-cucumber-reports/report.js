2fee14d897fa2cfcabaa90483d901e8ec43e8915
ServetAkguc <servet.akguc@gmail.com>
2020-11-14 19:30:20 -0700

    Adding feature file

---
+++ target/default-cucumber-reports/report.js
@@ -0,0 +1,177 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/TC_005.feature");
+formatter.feature({
+  "name": "Login page should not be accessible with invalid credentials",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "user navigates to GMI bank application",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "click on sign button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "Login with invalid username",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "User cannot login with invalid username validating the error message",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "user navigates to GMI bank application",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "click on sign button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "Login with invalid password",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "User cannot login with invalid password validating the error message",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "user navigates to GMI bank application",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "click on sign button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "invalid username and password",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "User cannot login with invalid username and password validating the error message",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "user navigates to GMI bank application",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "click on sign button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "given an option to reset their password",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "User with invalid credentials should be given an option to reset their password",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.background({
+  "name": "",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.step({
+  "name": "user navigates to GMI bank application",
+  "keyword": "Given "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.step({
+  "name": "click on sign button",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "name": "given the option to navigate to registration page",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "User should be given the option to navigate to registration page if they did not register yet",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
